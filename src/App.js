4ded04bab24e176409ce56dd88785fb10003a225
wolstadxd <wolski.rd@gmail.com>
2023-01-17 14:19:10 +0200

    scss moudle, props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import Card from './components/Card'
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+    {
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 12999,
+        imageUrl: '/img/sneakers/1.jpg'
+    },
+    {
+    name: 'Мужские Кроссовки Nike Air Max 270',
+        price: 15400,
+        imageUrl: '/img/sneakers/2.jpg'
+    },
+    {
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 8500,
+        imageUrl: '/img/sneakers/3.jpg'
+    },
+    {
+    name: 'Кроссовки Puma X Aka Boku Future Rider',
+        price: 9600,
+        imageUrl: '/img/sneakers/4.jpg'
+    }
+]
+
 
 function App() {
 
@@ -25,10 +48,13 @@ function App() {
 
         <div className="d-flex flex-wrap">
 
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+            {arr.map((obj) => (
+                <Card
+                    title={obj.name}
+                    price={obj.price}
+                    imageUrl={obj.imageUrl}
+                />
+            ))}
 
         </div>
 
